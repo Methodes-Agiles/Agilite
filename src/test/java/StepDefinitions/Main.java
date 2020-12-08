fe42cb73d4ed18c65be71ceb39ac487b5298e336
Mya96 <44229449+Mya96@users.noreply.github.com>
2020-12-08 23:29:35 +0100

    Add files via upload

---
+++ src/test/java/StepDefinitions/Main.java
@@ -0,0 +1,10 @@
+package StepDefinitions;
+
+public class Main {
+	public static void main(String[] args) throws Throwable {
+
+		Main.main(new String[] { "-g", "src/test/java/StepDefinitions",
+				"src/test/resources/Features/MarinScenario.feature" });
+	}
+
+}
